657fc4667eb9e3b2079ac22bc40d6375a3a49d1d
Kobina21 <rkaforson@st.ug.edu.gh>
2024-07-10 00:00:44 +0000

    Create Program.cs

---
+++ Login_screen/Program.cs
@@ -0,0 +1,18 @@
+namespace Login_screen;
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            string username = usernameTextBox.Text;
+            string password = passwordTextBox.Text;
+
+            MessageBox.Show($"Username: {username}\nPassword: {password}", "Login Information");
+        }
+    }
+}
